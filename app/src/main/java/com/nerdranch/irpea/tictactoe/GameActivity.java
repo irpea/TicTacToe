ec1ccb51d3df0ffa101b57b83d7a20912cd686c8
irpea <irpea@live.com>
2017-07-24 21:58:34 +0200

    Initial commit

---
+++ app/src/main/java/com/nerdranch/irpea/tictactoe/GameActivity.java
@@ -0,0 +1,13 @@
+package com.nerdranch.irpea.tictactoe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
